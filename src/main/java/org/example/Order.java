7f92a0145a46d9020cbb7e8557e554d282b8da3b
Lee Vue <vuelee123@gmail.com>
2023-06-05 15:38:11 -0400

    order processing ....

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,23 +3,52 @@ package org.example;
 import java.util.Map;
 
 public class Order {
+    private static int nextOrderId = 1;
     private int orderID;
-    private Map<MenuItem, Integer> itemsOrdered;
+    private Map<String, Integer> itemsOrdered;
     private double totalPrice;
     private OrderStatus status;
 
-    public Order(int orderID, Map<MenuItem, Integer> itemsOrdered, double totalPrice, OrderStatus status) {
-        this.orderID = orderID;
+    public Order(Map<String, Integer> itemsOrdered) {
+        this.orderID = nextOrderId++;
         this.itemsOrdered = itemsOrdered;
-        this.totalPrice = totalPrice;
+        calculateTotalPrice();
+        this.status = OrderStatus.WAITING;
+    }
+    private void calculateTotalPrice() {
+        double price = 0.0;
+        for (Map.Entry<String, Integer> entry : itemsOrdered.entrySet()) {
+            String itemName = entry.getKey();
+            int quantity = entry.getValue();
+            double itemPrice = getItemPrice(itemName);
+            price += itemPrice * quantity;
+        }
+        this.totalPrice = price;
+    }
+    private double getItemPrice(String itemName) {
+
+        if (itemName.equals("Cheeseburger")) {
+            return 2.0;
+        } else if (itemName.equals("Fries")) {
+            return 1.0;
+        } else if (itemName.equals("Strawberry Shake")) {
+            return 1.50;
+        }
+        return 0.0;
+    }
+    public Integer getOrderID() {
+        return orderID;
+    }
+    public void setStatus(OrderStatus status) {
         this.status = status;
     }
-
-    public Integer getOrderID() {
-        return getOrderID();
-    }
-
-    public void setStatus(OrderStatus status) {
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderID  +
+                ", itemsOrdered=" + itemsOrdered +
+                ", totalPrice=" + totalPrice +
+                ", status=" + status +
+                '}';
     }
 
     // Getters, Setters, etc.
