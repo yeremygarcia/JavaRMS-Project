4f8b774c4e08746255b47a656b93cb55fbb4a305
Stephen Alfa <stojalfa@gmail.com>
2023-06-07 12:24:31 -0400

    Updated menu

--- src/main/java/org/example/model/Order.java
+++ src/main/java/org/example/model/Order.java
@@ -1,4 +1,4 @@
-package org.example.model;
+package org.example;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -7,10 +7,15 @@ public class Order {
     private double totalPrice;
     private OrderStatus orderStatus;
     private HashMap<String, Integer> quantityOrdered;
-    private List<Customer.MenuItem> itemsOrdered;
+    private List<MenuItem> itemsOrdered;
     private final Date lastHandled;
     private int orderID;
     private int orderTableID;
+    enum OrderStatus {
+        WAITING,
+        PREPARING,
+        READY
+    }
 
     public Order() {
         this.totalPrice = 0;
@@ -27,30 +32,15 @@ public class Order {
     private void calculateTotalPrice() {
         setTotalPrice();
         double price = 0.0;
-<<<<<<< HEAD:src/main/java/org/example/model/Order.java
-        for (Customer.MenuItem itemName : itemsOrdered) {
-            int quantity = getQuantity(itemName);
-            double itemPrice = getItemPrice(String.valueOf(itemName));
-=======
         for (Map.Entry<String, Integer> entry : itemsOrdered.entrySet()) {
             String itemName = entry.getKey();
             int quantity = entry.getValue();
             double itemPrice = getItemPrice(itemName);
->>>>>>> parent of 383799f... Got File I/O working again, this is the way:src/main/java/org/example/Order.java
             price += itemPrice * quantity;
         }
         this.totalPrice = price;
     }
-<<<<<<< HEAD:src/main/java/org/example/model/Order.java
-
-    private int getQuantity(Customer.MenuItem itemName) {
-        return 0;
-    }
-
-    public void addItemsOrdered(Customer.MenuItem item) {
-=======
     public void addItemsOrdered(MenuItem item) {
->>>>>>> parent of 383799f... Got File I/O working again, this is the way:src/main/java/org/example/Order.java
         totalPrice = 0;
         itemsOrdered.add(item);
         getTotalPrice();
@@ -89,28 +79,28 @@ public class Order {
         return (Map<String, Integer>) itemsOrdered;
     }
     public String getOrderedItems(){
-        List<Customer.MenuItem> itemsOrdered = getItemsOrderedList();
+        List<MenuItem> itemsOrdered = getItemsOrderedList();
         String orderList = "";
-        for(Customer.MenuItem item: itemsOrdered){
+        for(MenuItem item: itemsOrdered){
             orderList += item + "\n";
         }
         return orderList;
     }
 
-    private List<Customer.MenuItem> getItemsOrderedList() {
+    private List<MenuItem> getItemsOrderedList() {
         return itemsOrdered;
     }
     public String addItemQuantity(){
         String orderInfo = "\n";
         quantityOrdered.clear();
-        for(Customer.MenuItem item: itemsOrdered){
+        for(MenuItem item: itemsOrdered){
             if(!quantityOrdered.containsKey(item.getName())){
                 quantityOrdered.put(item.getName(),1);
             } else {
                 quantityOrdered.put(item.getName(), quantityOrdered.get(item.getName()) + 1);
             }
         }
-        for(Customer.MenuItem item: itemsOrdered){
+        for(MenuItem item: itemsOrdered){
             if(!orderInfo.contains(item.getName())){
                 orderInfo += item.getName() + "->" + quantityOrdered.get(item.getName()) + "\n";
             }
