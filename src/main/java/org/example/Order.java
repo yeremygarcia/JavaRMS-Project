20e530141580a6f89e48cf9fb46dc9d57149aa1f
Stephen Alfa <stojalfa@gmail.com>
2023-06-08 12:07:08 -0400

    Updating Order Processing

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,128 +1,86 @@
 package org.example;
 
+import org.example.OrderStatus;
 import org.example.model.MenuItem;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Order {
-    private double totalPrice;
-    private OrderStatus orderStatus;
-    private HashMap<String, Integer> quantityOrdered;
-    private List<MenuItem> itemsOrdered;
-    private final Date lastHandled;
     private int orderID;
     private int orderTableID;
+    private double totalPrice;
+    private OrderStatus orderStatus;
+    private Map<MenuItem, Integer> itemsOrdered;
+    private Date lastHandled;
 
-    public enum OrderStatus {
-        WAITING,
-        PREPARING,
-        READY
-    }
 
-    public Order() {
-        this.totalPrice = 0;
-        this.orderID = Integer.parseInt(String.valueOf(0));
-        //this.orderStatus = OrderStatus.WAITING;
-        this.itemsOrdered = new ArrayList<>();
-        this.quantityOrdered = new HashMap<>();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    public Order(int orderID, int orderTableID) {
+        this.orderID = orderID;
+        this.orderTableID = orderTableID;
+        this.totalPrice = 0.0;
+        this.orderStatus = OrderStatus.WAITING;
+        this.itemsOrdered = new HashMap<>();
         this.lastHandled = new Date();
-        formatter.format(lastHandled);
-        this.orderTableID = 0;
-    }
-
-    private void calculateTotalPrice() {
-        setTotalPrice();
-        double price = 0.0;
-        for (MenuItem itemName : itemsOrdered) {
-            int quantity = getQuantity(itemName);
-            double itemPrice = getItemPrice(String.valueOf(itemName));
-            price += itemPrice * quantity;
-        }
-        this.totalPrice = price;
-    }
-
-    private int getQuantity(MenuItem itemName) {
-        return 0;
-    }
-
-    public void addItemsOrdered(MenuItem item) {
-        totalPrice = 0;
-        itemsOrdered.add(item);
-        getTotalPrice();
-    }
-
-    private void setTotalPrice() {
-    }
-
-    private double getItemPrice(String itemName) {
-        if (itemName.equals("Cheeseburger")) {
-            return 2.0;
-        } else if (itemName.equals("Fries")) {
-            return 1.0;
-        } else if (itemName.equals("Strawberry Shake")) {
-            return 1.50;
-        }
-        return 0.0;
     }
 
     public int getOrderID() {
         return orderID;
     }
-    public void setOrderID(int i) {
+
+    public int getOrderTableID() {
+        return orderTableID;
     }
 
-    public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-    public OrderStatus getOrderStatus() {
-        return orderStatus;
-    }
     public double getTotalPrice() {
-        setTotalPrice();
         return totalPrice;
     }
 
-    public Map<String, Integer> getItemsOrdered() {
-        return (Map<String, Integer>) itemsOrdered;
-    }
-    public String getOrderedItems(){
-        List<MenuItem> itemsOrdered = getItemsOrderedList();
-        String orderList = "";
-        for(MenuItem item: itemsOrdered){
-            orderList += item + "\n";
-        }
-        return orderList;
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
     }
 
-    private List<MenuItem> getItemsOrderedList() {
+    public Map<MenuItem, Integer> getItemsOrdered() {
         return itemsOrdered;
     }
-    public String addItemQuantity(){
-        String orderInfo = "\n";
-        quantityOrdered.clear();
-        for(MenuItem item: itemsOrdered){
-            if(!quantityOrdered.containsKey(item.getName())){
-                quantityOrdered.put(item.getName(),1);
-            } else {
-                quantityOrdered.put(item.getName(), quantityOrdered.get(item.getName()) + 1);
-            }
-        }
-        for(MenuItem item: itemsOrdered){
-            if(!orderInfo.contains(item.getName())){
-                orderInfo += item.getName() + "->" + quantityOrdered.get(item.getName()) + "\n";
-            }
-        }
-        return orderInfo;
+
+    public Date getLastHandled() {
+        return lastHandled;
     }
 
+    public void addItem(MenuItem item, int quantity) {
+        itemsOrdered.put(item, quantity);
+        calculateTotalPrice();
+    }
+
+    public void removeItem(MenuItem item) {
+        itemsOrdered.remove(item);
+        calculateTotalPrice();
+    }
+
+    public void updateOrderStatus(OrderStatus status) {
+        this.orderStatus = status;
+    }
+
+    private void calculateTotalPrice() {
+        double price = 0.0;
+        for (Map.Entry<MenuItem, Integer> entry : itemsOrdered.entrySet()) {
+            MenuItem item = entry.getKey();
+            int quantity = entry.getValue();
+            price += item.getPrice() * quantity;
+        }
+        this.totalPrice = price;
+    }
+
+    @Override
     public String toString() {
         return "Order{" +
-                "orderID='" + orderID + '\'' +
-                ", itemsOrdered=" + itemsOrdered +
+                "orderID=" + orderID +
+                ", orderTableID=" + orderTableID +
                 ", totalPrice=" + totalPrice +
-                ", status=" + orderStatus +
+                ", orderStatus=" + orderStatus +
+                ", itemsOrdered=" + itemsOrdered +
+                ", lastHandled=" + lastHandled +
                 '}';
     }
 }
--- src/main/java/org/example/OrderService.java
+++ src/main/java/org/example/OrderService.java
@@ -7,34 +7,47 @@ import java.util.Map;
 
 public class OrderService {
     private List<Order> orderList;
-    private Map<Integer, Order> activeOrders = new HashMap<>();
+    private Map<Integer, Order> activeOrders;
     private int totalOrders;
+
     public OrderService() {
         this.orderList = new ArrayList<>();
+        this.activeOrders = new HashMap<>();
         this.totalOrders = 0;
     }
+
     public void addOrder(Order order) {
         orderList.add(order);
-        totalOrders = orderList.size();
-        activeOrders.put(Integer.valueOf(order.getOrderID()), order);
+        activeOrders.put(order.getOrderID(), order);
+        totalOrders++;
     }
 
     public void removeOrder(int orderID) {
-        activeOrders.remove(orderID);
+        Order order = activeOrders.remove(orderID);
+        if (order != null) {
+            orderList.remove(order);
+            totalOrders--;
+        }
     }
 
     public Order getOrder(int orderID) {
         return activeOrders.get(orderID);
     }
-    public String updateStatus(Order.OrderStatus status){
-        String foundOrders = "";
-        for (Order order: orderList){
-            if(order.getOrderStatus() == status){
-                foundOrders += order + "\n";
+
+    public List<Order> getAllOrders() {
+        return orderList;
+    }
+
+    public List<Order> getOrdersByStatus(OrderStatus status) {
+        List<Order> ordersByStatus = new ArrayList<>();
+        for (Order order : orderList) {
+            if (order.getOrderStatus() == status) {
+                ordersByStatus.add(order);
             }
         }
-        return foundOrders;
+        return ordersByStatus;
     }
+
     public int getTotalOrders() {
         return totalOrders;
     }
